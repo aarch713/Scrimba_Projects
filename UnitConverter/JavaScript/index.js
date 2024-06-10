263e7fa574b9288e3b7f9d33978deb1eb534adad
aarch713 <aarch.code01@gmail.com>
2024-06-10 15:04:59 -0700

    New Project shopping list

--- UnitConverter/JavaScript/index.js
+++ UnitConverter/JavaScript/index.js
@@ -11,14 +11,14 @@ btnEl.addEventListener('click',writeValue)
 function writeValue () {
     let num = Number(inputNum.value)
 
-    alert(typeof num)
+    // alert(typeof num)
     let calvalLen1 = num * 3.281
     let calvalLen2 = num / 3.281
     let calvalVol1 = num * 0.264
     let calvalVol2 = num / 0.264
     let calvalMas1 = num * 2.204
     let calvalMas2 = num / 2.204
-    if((typeof inputNum.value) === "number"){
+    if((typeof inputNum.value) !== "number"){
         outputLength.textContent = "AInvalid input, please enter number only.";
     }
     // else if(inputNum.value === String){
