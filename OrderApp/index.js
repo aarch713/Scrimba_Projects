65db70ee054dd6fc167fa087008dcd98d6f3206e
aarch713 <95728295+aarch713@users.noreply.github.com>
2024-06-18 22:10:16 -0700

    Daily

---
+++ OrderApp/index.js
@@ -0,0 +1,10 @@
+import {menuArray} from "./data.js"
+
+const incrementBtn = document.getElementById("incrementBtn")
+const itemNum = document.getElementById("ordernumValue")
+
+incrementBtn.addEventListener('click', () => console.log(itemNum.textContent))
+
+
+
+
