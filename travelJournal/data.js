37f3e421cf6906ec1319865a846a0468d96ac23b
aarch713 <95728295+aarch713@users.noreply.github.com>
2024-07-13 01:33:39 -0700

    TravelProject

---
+++ travelJournal/data.js
@@ -0,0 +1,47 @@
+export default [
+    {
+        title: "Mount Fuji",
+        location: "Japan",
+        googleMapsUrl: "https://maps.app.goo.gl/9qSfcudjh7x6gsR86",
+        startDate: "12 Jan, 2021",
+        endDate: "24 Jan, 2021",
+        description: "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
+        imageUrl: "https://images.unsplash.com/photo-1490806843957-31f4c9a91c65?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    },
+    {
+        title: "Eiffel Tower",
+        location: "France",
+        googleMapsUrl: "https://maps.app.goo.gl/Pkn4jnRNJ5Kr766a6",
+        startDate: "15 Apr, 2021",
+        endDate: "22 Apr, 2021",
+        description: "The Eiffel Tower is an iconic landmark located in Paris, France. Standing at 324 meters (1,063 feet), it offers stunning views of the city and is a popular destination for tourists from around the world.",
+        imageUrl: "https://images.unsplash.com/photo-1604175287072-b5e71423060c?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    },
+    {
+        title: "Great Wall of China",
+        location: "China",
+        googleMapsUrl: "https://maps.app.goo.gl/BBvWkhUm5r4JKbZ37",
+        startDate: "10 May, 2021",
+        endDate: "20 May, 2021",
+        description: "The Great Wall of China is a historic fortification stretching over 13,000 miles. It is a symbol of ancient Chinese engineering and a UNESCO World Heritage site, attracting millions of visitors each year.",
+        imageUrl: "https://images.unsplash.com/photo-1608037521244-f1c6c7635194?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    },
+    {
+        title: "Machu Picchu",
+        location: "Peru",
+        googleMapsUrl: "https://maps.app.goo.gl/XHanmQcduRLkXR8P7",
+        startDate: "01 Jun, 2021",
+        endDate: "10 Jun, 2021",
+        description: "Machu Picchu is an ancient Incan city located in the Andes Mountains. Known for its archaeological significance and breathtaking views, it is a must-visit destination for history and nature enthusiasts.",
+        imageUrl: "https://images.unsplash.com/photo-1592366610190-949bbcabccb2?q=80&w=2067&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    },
+    {
+        title: "Sydney Opera House",
+        location: "Australia",
+        googleMapsUrl: "https://maps.app.goo.gl/9p5YyNAFajHCCK5z7",
+        startDate: "25 Jul, 2021",
+        endDate: "31 Jul, 2021",
+        description: "The Sydney Opera House is a world-renowned performing arts center located on Sydney Harbour. Its distinctive sail-like design makes it one of the most recognizable buildings in the world.",
+        imageUrl: "https://images.unsplash.com/photo-1616128618694-96e9e896ecb7?q=80&w=1980&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    }
+]
