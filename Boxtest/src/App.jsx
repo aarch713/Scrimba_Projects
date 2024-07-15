f12fb9281d0c42d1d639bab3cca060fd05bda0fa
aarch713 <95728295+aarch713@users.noreply.github.com>
2024-07-15 00:18:48 -0700

    daily

---
+++ Boxtest/src/App.jsx
@@ -0,0 +1,32 @@
+// import { useState } from 'react'
+import React from 'react'
+import Box from './Box'
+import boxes from './boxes.js'
+export default function App() {
+  const [squares, setSquares] = React.useState(boxes)
+  console.log('Im in App')
+  function toggle(id) {
+    setSquares(prevSquares => {
+        return prevSquares.map((square) => {
+            return square.id === id ? {...square, on: !square.on} : square
+        })
+    })
+}
+
+
+    
+const squareElements = squares.map(square => (
+  <Box 
+      key={square.id} 
+      on={square.on} 
+      toggle={() => toggle(square.id)}
+  />
+))
+
+
+  return (
+    <main>
+      {squareElements}
+    </main>
+  )
+}
