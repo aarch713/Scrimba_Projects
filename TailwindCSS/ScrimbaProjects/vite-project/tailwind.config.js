6c33c66e976993d79ef8ff8752f9e4dd93131fa0
aarch713 <aarch.code01@gmail.com>
2024-11-20 12:32:12 -0800

    daily

--- TailwindCSS/ScrimbaProjects/vite-project/tailwind.config.js
+++ TailwindCSS/ScrimbaProjects/vite-project/tailwind.config.js
@@ -3,11 +3,10 @@ export default {
   content: [
     "./index.html",
     "./src/**/*.{js,jsx,ts,tsx}",
-    "./*.{js,jsx,ts,tsx}"
+    "./*.{js,jsx,ts,tsx}",
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+};
